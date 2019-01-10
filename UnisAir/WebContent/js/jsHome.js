7ca15509729710b237e00f2bef93b08fcc52573e
mario-santoro <43414100+mario-santoro@users.noreply.github.com>
2019-01-10 09:28:27 +0100

    Add files via upload

--- UnisAir/WebContent/js/jsHome.js
+++ UnisAir/WebContent/js/jsHome.js
@@ -49,8 +49,8 @@ $(window).scroll(function() {
 	
 })
 //controllo min e max della data di partenza e ritorno
-function dat(){
-var now = new Date();
+
+	var now = new Date();
     var day = ("0" + now.getDate()).slice(-2);
     var month = ("0" + (now.getMonth() + 1)).slice(-2);
     var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
@@ -64,7 +64,8 @@ var now = new Date();
     	var min=  document.getElementById('datPartenza').value;
     	document.getElementById('datRitorno').min =min;
 	
-    }}
+    }
+
 //incremento e decremento posti di adulti e bambini tramite pulsanti
 	function incrementAdult(){
 		var val = document.getElementById("adultNumber").value;
